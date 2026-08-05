1d76a3c76c2d584e8b8d9be3fb6a26ae915d3c7e
Balmiki Badatya <balmikib830@outlook.com>
2026-08-04 19:30:56 -0700

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void sort(int[] nums, int left, int right) {
+        if (left == right) {
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        sort(nums, left, mid);
+        sort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        List<Integer> temp = new ArrayList<>();
+
+        int low = left;
+        int high = mid + 1;
+
+        while (low <= mid && high <= right) {
+            if (nums[low] < nums[high]) {
+                temp.add(nums[low]);
+                low++;
+            } else {
+                temp.add(nums[high]);
+                high++;
+            }
+        }
+
+        while (low <= mid) {
+            temp.add(nums[low]);
+            low++;
+        }
+
+        while (high <= right) {
+            temp.add(nums[high]);
+            high++;
+        }
+
+        int index = 0;
+        for (int i = left; i <= right; i++) {
+            nums[i] = temp.get(index);
+            index++;
+        }
+    }
+}
